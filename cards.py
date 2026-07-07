1e45d690022e9df2fc0ca4802fbcfeed19086ee7
xenon54xenon54 <ethanmay03@gmail.com>
2026-07-07 18:37:34 -0400

    implemented `Deck` class; essentially a set, but `pop` is renamed to `draw`

--- cards.py
+++ cards.py
@@ -1,8 +1,14 @@
 from random import randint
+from typing import assert_type
 
+from sympy import rem
 
-ranks = ("a", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k")
-suits = ("spades", "clubs", "hearts", "diamonds")
+numberRanks = ["2", "3", "4", "5", "6", "7", "8", "9", "10"]
+faceRanks = ["j", "q", "k"]
+ranks = tuple(["a"] + numberRanks + faceRanks)
+black_suits = ["spades", "clubs"]
+red_suits = ["hearts", "diamonds"]
+suits = tuple(black_suits + red_suits)
 editions = ("base", "foil", "holographic", "polychrome")
 seals = ("base", "gold", "red", "blue", "purple")
 enhancements = ("base", "bonus", "mult", "wild", "glass", "steel", "stone", "gold", "lucky")
@@ -73,3 +79,60 @@ class PlayingCard:
             elif self.edition == "polychrome": m *= 1.5
         self._mults = m
         return self._mults
+
+
+deckTypes = ("base", "abandoned", "checkered", "random") #verify name of last deck type
+
+class Deck:
+    def __init__(self, cardSet: set[PlayingCard]) -> None:
+        self._cards = cardSet
+        self.remaining = len(cardSet)
+    
+    @property
+    def cards(self):
+        """
+        Returns the set of `PlayingCard`s within the deck
+        Does NOT affect the contents
+        """
+        return self._cards
+    @cards.setter
+    def cards(self, newCards: set[PlayingCard]) -> set[PlayingCard]:
+        """
+        Adds the set of `PlayingCard`s to the deck
+        Returns the contents of the deck, including new additions
+        """
+        return self._cards.union(newCards)
+    @cards.deleter
+    def cards(self, remCard: PlayingCard):
+        """
+        Removes the specified `PlayingCard` object from the deck
+        Throws an error if no such card was in the deck
+        """
+        self._cards.remove(remCard)
+    @cards.getter
+    def draw(self) -> PlayingCard:
+        """
+        Removes an arbitrary `PlayingCard` from the deck and returns it
+        Throws an error if the deck is empty
+        """
+        return self._cards.pop() #this might be very bad rng, but it's easy
+
+
+def generateDeck(d: str = "base") -> set[PlayingCard]:
+    assert d in deckTypes
+    cardSet = set()
+    if d != "random":
+        filteredRanks = ranks
+        filteredSuits = suits
+        if d != "base":
+            if d == "abandoned": filteredRanks = ["a"] + numberRanks
+            elif d == "checkered": filteredSuits = ["spades", "hearts"] * 2
+        for r in filteredRanks:
+            for s in filteredSuits:
+                #different objects get hashed separately, even if they have the same attributes
+                cardSet.add(PlayingCard(r, s))
+    else: 
+        #TODO
+        #need to look at how Balatro generates it
+        pass
+    return cardSet
