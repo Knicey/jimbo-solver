33a228f73e5c685894ccd15bbe55ee5f223058ec
xenon54xenon54 <ethanmay03@gmail.com>
2026-07-22 14:17:50 -0400

    migrated test cases for determine_highest_hand_ranking

--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
 from cards import fromStr
-from calculator import determine_highest_hand_ranking, repHands
+from calculator import determine_highest_hand_ranking, pokerHands
 
-#consider storing this as separate JSON(s)
+# consider storing this as separate JSON(s)
 cases = {
-    "fromStr" : {
+    "fromStr": {
         "1h": ("a", "hearts"),
         "10h": ("10", "hearts"),
         "1hh": ("a", "hearts", "holographic"),
@@ -21,20 +21,33 @@ cases = {
         "kings puluck": ("k", "spades", "base", "purple", "lucky"),
         "KSPAD-pl": ("k", "spades", "base", "purple", "lucky"),
     },
-    "determine_highest_hand_ranking" : {
-        "12H,13D,12D,12H":repHands[3]
+    "determine_highest_hand_ranking": {
+        "12H,13D,12D,12H": pokerHands.repHands[3],
+        "12H,12H,12H,12H,12H": pokerHands.flushFive,
+        "12H,12H,12H,13H,13H": pokerHands.fullHouses[True],
+        "12H,12H,13H,13H,14H": pokerHands.flush,
+        "09H,10H,11H,12H,13H": pokerHands.straights[True],
+        "09H,10H,11H,12H,13C": pokerHands.straights[False],
+        "09H,09H,09C,12D,12D": pokerHands.fullHouses[False],
+        "14H,14D,13D,11H": pokerHands.repHands[2],
+        "14H,14D,11D,11H": pokerHands.twoPair,
+        "01H,02H,03H,04H,05C": pokerHands.straights[False],
+        "10S,jH ,qD ,kD ,aH ": pokerHands.straights[False],
+        "1h ,1h ,1h ,1h ,1cw": pokerHands.flushFive,
     }
 }
 
+
 class test_fromStr:
     name = "fromStr"
+
     def __init__(self, testCases: dict[str, tuple[str, str]] = cases[name]) -> None:
         self.testCases = testCases
-    
+
     def validate(self):
         for k, v in self.testCases.items():
             card = fromStr(k)
-            #print(card, v)
+            # print(card, v)
             assert card.rank == v[0]
             assert card.suit == v[1]
             if len(v) >= 3:
@@ -51,8 +64,10 @@ class test_fromStr:
                 assert card.enhancement == "base"
         return True
 
+
 class test_determine_highest_hand_ranking:
     name = "determine_highest_hand_ranking"
+
     def __init__(self, testCases: dict[str, str] = cases[name]) -> None:
         self.testCases = testCases
 
@@ -62,6 +77,7 @@ class test_determine_highest_hand_ranking:
             result = determine_highest_hand_ranking(playedHand)
             assert result == v
         return True
+
 
 tests = [test_fromStr(), test_determine_highest_hand_ranking()]
 verbose = True
